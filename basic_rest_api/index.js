b89039e4f9767d25d0df5e180d58910fcdb4cc14
Highoncaffiene <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-02-09 10:15:07 +0530

    get and post

---
+++ basic_rest_api/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // .use() sets up a middleware
+
+// app.use(express.json())
+const PORT = 3000;
+
+//mimic the db using an array
+let blogList = [];
+app.get('/blogs', (req, res)=>{
+    return res.status(200).json({ // returns the response object, but also with a status code in it.
+        data:blogList,
+        success: true,
+        // code:200
+    });
+});
+
+app.post('/blogs', (req, res)=>{
+    // console.log(req.body)
+    blogList.push({title: req.body.title,
+         content: req.body.content,
+        id : Math.floor(Math.random()*1000)
+    });
+    return res.status(201).json({
+        success: true
+    })
+})
+
+app.get('/blogs/:id', (req,res)=>{
+    const result = blogList.filter((blog)=> blog.id==req.params.id)
+    return req.status(200).json({
+        data: result,
+        success : true
+})
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server started on port", PORT);
+})
+
+//Hot reloading- whenever u make any change in the codebase, the server automatically restarts( one of the package to do this is nodemon, 
+// but new version of node also provides this feature by default)
+
+//npx-> executes npm package binaries
