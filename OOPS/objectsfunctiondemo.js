c0a6d99c270c7095cabd04cf26f3ea31528f5fe3
Highoncaffiene <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-02-13 19:50:50 +0530

    object methods

---
+++ OOPS/objectsfunctiondemo.js
@@ -0,0 +1,14 @@
+let obj = {
+    x:1, y:2, z:3
+}
+
+let keys = Object.keys(obj) // returns array of keys 
+console.log(keys);
+
+let value = Object.values(obj) // returns array of values
+console.log(value);
+
+
+let entries = Object.entries(obj) // returns arraay of key value pair of object
+
+console.log(entries);
