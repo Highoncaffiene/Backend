919c342b0ceb72210ddde08c24df47a256c9e310
Highoncaffiene <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-02-13 14:37:11 +0530

    notes improved

--- OOPS/binddemo.js
+++ OOPS/binddemo.js
@@ -8,15 +8,15 @@ const player1 = {
     }
 }
 
-// const obj = function(){
-    // console.log(this.getdetails()); // doesn't have the context of this   
-// }
+const obj = function(){
+    console.log(this.getdetails()); // doesn't have the context of this   
+}
 
 // obj();
-// let x = obj.bind(player1)// creats a new function where this is permanently set to the plaayer1
-// x(); // to obj function, and as "this" is set to player1 permanently, so this.getdetails() goes to getdetails key in player1, so finally we have plaayer1.getdetails() inside console.log()
+let x = obj.bind(player1)// creats a new function where this is permanently set to the plaayer1
+x(); // to obj function, and as "this" is set to player1 permanently, so this.getdetails() goes to getdetails key in player1, so finally we have plaayer1.getdetails() inside console.log()
 // and inside getdetails(), this is set to player1, and this prints player1.firstname and so on and so forth  
-// console.log(x)
+console.log(x)
 
 const obj2 = function(x,y){
     console.log(this.getdetails())
