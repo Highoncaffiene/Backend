448836c1c5497ea4eb18ba88f7a5df8daf74dc2f
Highoncaffiene <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-02-14 19:22:22 +0530

    inheritance1

---
+++ OOPS/inheritance1.js
@@ -0,0 +1,28 @@
+class Category{
+    constructor(c){
+        this.categoryname = this.categoryname;
+    }
+
+    getCategoryName(){
+        console.log("category is", this.categoryname)
+    }
+}
+
+class Product extends Category{
+    constructor(n,p,c){
+        super(c)
+        this.name = n;
+        this.price =p;
+    }
+
+    display(){
+        console.log("Details of the product are")
+        console.log("Name", this.name);
+        console.log("Price", this.price);  
+        this.getCategoryName()
+        
+    }
+}
+
+let p = new Product("Iphone", 100000, "Electronics")
+p.display()
